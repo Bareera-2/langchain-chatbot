af0c3a9ad7f16315e4703f21faa9ba90f017b80e
Bareera Mahmud <bareeramahmud22@gmail.com>
2026-03-06 10:07:31 +0500

    update all files

--- test_module.py
+++
@@ -1,9 +0,0 @@
-class Student:
-    # Magic or Constructor Method
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email
-    
-    # instance method
-    def info(self):
-        return f"Name {self.name}, Email {self.email}"
